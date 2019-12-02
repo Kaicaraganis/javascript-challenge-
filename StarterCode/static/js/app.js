d44b849870eaface1c42cf4ff7d099a8fb01dfa1
Kaicaraganis <53307678+Kaicaraganis@users.noreply.github.com>
2019-12-02 18:15:43 -0500

    Add files via upload

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,29 @@
+// from data.js
+var data = data;
+var tbody = d3.select("tbody")
+​
+// loop and add table values 
+data.forEach(function(ufo){
+    var row = tbody.append("tr");
+    Object.entries(ufo).forEach(function([key,value]){
+        var cell = row.append("td").text(value);
+    });
+});
+​
+// button for filter  
+var button = d3.select("filter-btn");
+button.on("click", function(){
+    var inputElement = d3.select(".form-control");
+    var inputDate = inputElement.property("value");
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputDate);
+    var tbody = d3.select("tbody")
+    tbody.html("");
+​
+    // loop and final filter values 
+    filteredData.forEach(function(ufo){
+        var row = tbody.append("tr");
+        Object.entries(ufo).forEach(function([key, value]){
+            var cell = row.append("td").text(value);
+        })
+    })
+});
